243b82312a1183b2c93356533bdb45fac59ad489
Eric Kreutzer <eric@airkrft.com>
2012-04-10 14:59:49 -0600

    Search shit

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,1 +1,14 @@
-Tire.configure { url ENV['BONSAI_INDEX_URL'] }
+require 'uri'
+
+if ENV['BONSAI_INDEX_URL'].present?
+  uri = URI( ENV['BONSAI_INDEX_URL'] )
+  Tire.configure {
+    url "#{uri.scheme}://#{uri.host}"
+  }
+
+  $bonsai_index_name = uri.path[/[^\/]+$/]
+else
+  app_name = Rails.application.class.parent_name.underscore.dasherize
+  app_env = Rails.env
+  $bonsai_index_name = "#{app_name}-#{app_env}"
+end
