1731bae41e9a3f5f129238657a3d6dc6ef6f7b26
Eric Kreutzer <eric@airkrft.com>
2012-04-10 15:44:19 -0600

    Search

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -4,11 +4,6 @@ if ENV['BONSAI_INDEX_URL'].present?
   uri = URI( ENV['BONSAI_INDEX_URL'] )
   Tire.configure {
     url "#{uri.scheme}://#{uri.host}"
+    global_index_name uri.path[/[^\/]+$/]
   }
-
-  $bonsai_index_name = uri.path[/[^\/]+$/]
-else
-  app_name = Rails.application.class.parent_name.underscore.dasherize
-  app_env = Rails.env
-  $bonsai_index_name = "#{app_name}-#{app_env}"
 end
