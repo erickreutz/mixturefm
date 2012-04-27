03abfc697d297159671d2b4233295ce121968808
Eric Kreutzer <eric@airkrft.com>
2012-04-27 17:55:49 -0600

    Fixed observers calling published?

--- app/observers/mix_collection_observer.rb
+++ app/observers/mix_collection_observer.rb
@@ -1,7 +1,7 @@
 class MixCollectionObserver < Mongoid::Observer
 	def after_update(mix_collection)
 		mix_collection.mixes.each do |mix|
-			mix.update_index if mix.published?
+			mix.update_index
 		end
 	end
 end
--- app/observers/performer_observer.rb
+++ app/observers/performer_observer.rb
@@ -1,7 +1,7 @@
 class PerformerObserver < Mongoid::Observer
 	def after_update(performer)
 		performer.mixes.each do |mix|
-			mix.update_index if mix.published?
+			mix.update_index
 		end
 	end
 end
