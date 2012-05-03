2a12b1393271bd270fcc4b8c5efbde8749b269e8
Eric Kreutzer <eric@airkrft.com>
2012-05-02 18:19:24 -0600

    Rwmoved elastic search initializer. Dont need anymore

--- config/initializers/elastic_search.rb
+++
@@ -1,9 +0,0 @@
-require 'uri'
-
-if ENV['BONSAI_INDEX_URL'].present?
-  uri = URI( ENV['BONSAI_INDEX_URL'] )
-  Tire.configure {
-    url "#{uri.scheme}://#{uri.host}"
-    global_index_name uri.path[/[^\/]+$/]
-  }
-end
