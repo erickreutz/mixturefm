e535f77d13c34f499afed1371168245b4b1052bf
Eric Kreutzer <eric@airkrft.com>
2012-05-21 15:21:54 -0600

    Update asset version

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,6 @@ module Mixture
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.assets.enabled = true
-    config.assets.version = '1.0.1'
+    config.assets.version = '1.0.2'
   end
 end
